8af19fb2f8555baf2cd57760928c7dda45e3d356
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-07-27 17:49:59 +0200

    milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
 function getMoviesListener() {
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie?',
-    method: 'GET',
-    data: {
+  var searchBtn = $('#send');
+  searchBtn.click(function () {
+    var input = $("#serchbar");
+    var txt = input.val();
+    var replacedTxt = txt.split(' ').join('+');
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie?',
+      method: 'GET',
+      data: {
         api_key: '95a07c6aeef2a2b492c138cc8e8c0eeb',
-        query: 'avengers'
-    },
-    success: function (data, state) {
-      var arrayMovies = data['results'];
+        query: replacedTxt
+      },
+      success: function (data, state) {
+        var arrayMovies = data['results'];
 
         var template = $('#movie-template').html();
         var compiled = Handlebars.compile(template);
         var target = $('.movies-container');
-
+        target.html('');
         for (var i = 0; i < arrayMovies.length; i++) {
           var movie = arrayMovies[i];
           console.log(movie);
           var movieHTML = compiled(movie);
           target.append(movieHTML);
         }
-    },
-    error: function (request, state, error) {
-    console.log('request', request);
-    console.log('state', state);
-    console.log('error', error);
-  }
+      },
+      error: function (request, state, error) {
+        console.log('request', request);
+        console.log('state', state);
+        console.log('error', error);
+      }
+    });
   });
+
+
 }
 
 
